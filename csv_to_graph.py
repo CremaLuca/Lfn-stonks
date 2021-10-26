5fa40ab0c2a44738eecc216b8b08fcf75cf0d90a
Luca <lc.crema@hotmaill.com>
2021-10-26 10:42:07 +0200

    refactor: switched to pandas

--- csv_to_graph.py
+++ csv_to_graph.py
@@ -6,10 +6,11 @@ __author__ = "Luca Crema"
 __version__ = "0.1"
 
 import argparse
-import csv
 import logging
-import re
 from typing import Dict, List
+
+import networkx as nx
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -60,11 +61,11 @@ EXPECTED_COLUMNS: List[str] = [
 ]
 
 # Define default CSV columns indexes
-DEFAULT_ETF_TICKER_COLUMN: int = EXPECTED_COLUMNS.index("Composite Ticker")
-DEFAULT_COMPONENT_TICKER_COLUMN: int = EXPECTED_COLUMNS.index("Constituent Ticker")
-DEFAULT_MARKET_VALUE_COLUMN: int = EXPECTED_COLUMNS.index("Market Value")
-DEFAULT_CURRENCY_COLUMN: int = EXPECTED_COLUMNS.index("Currency")
-DEFAULT_ISIN_COLUMN: int = EXPECTED_COLUMNS.index("ISIN")
+DEFAULT_ETF_TICKER_COLUMN: str = "Composite Ticker"
+DEFAULT_COMPONENT_TICKER_COLUMN: str = "Constituent Ticker"
+DEFAULT_MARKET_VALUE_COLUMN: str = "Market Value"
+DEFAULT_CURRENCY_COLUMN: str = "Currency"
+DEFAULT_ISIN_COLUMN: str = "ISIN"
 
 # Currency conversion rates
 CURRENCY_CONVERSION_RATES: Dict[str, float] = {
@@ -95,7 +96,7 @@ def make_parser() -> argparse.ArgumentParser:
         metavar="PATH",
         type=str,
         nargs=1,
-        default="out_graph.edgeList",
+        default=["out_graph"],
         help="path of the output file, default `out_graph.edgeList`.",
     )
     parser.add_argument(
@@ -114,55 +115,51 @@ def make_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "-log", "--loglevel", type=str, nargs=1, default=["warning"], help="provide logging level, default `warning`."
     )
+    # CSV columns group
     csv_columns_group = parser.add_argument_group("CSV columns", "Location of the information in the CSV columns.")
     csv_columns_group.add_argument(
         "--etf-ticker-column",
-        metavar="INT",
-        type=int,
+        metavar="STR",
+        type=str,
         nargs="?",
         default=DEFAULT_ETF_TICKER_COLUMN,
-        help=f"index of the column containing the ETF ticker, default `{DEFAULT_ETF_TICKER_COLUMN}`.",
+        help=f"name of the column containing the ETF ticker, default `{DEFAULT_ETF_TICKER_COLUMN}`.",
     )
     csv_columns_group.add_argument(
         "--component-ticker-column",
-        metavar="INT",
-        type=int,
+        metavar="STR",
+        type=str,
         nargs="?",
         default=DEFAULT_COMPONENT_TICKER_COLUMN,
-        help=f"index of the column containing the component ticker, default `{DEFAULT_COMPONENT_TICKER_COLUMN}`.",
+        help=f"name of the column containing the component ticker, default `{DEFAULT_COMPONENT_TICKER_COLUMN}`.",
     )
     csv_columns_group.add_argument(
         "--market-value-column",
-        metavar="INT",
-        type=int,
+        metavar="STR",
+        type=str,
         nargs="?",
         default=DEFAULT_MARKET_VALUE_COLUMN,
-        help=f"index of the column containing the amount of $currency the ETF has of the component, \
+        help=f"name of the column containing the amount of $currency the ETF has of the component, \
             default `{DEFAULT_MARKET_VALUE_COLUMN}`.",
     )
     csv_columns_group.add_argument(
         "--currency-column",
-        metavar="INT",
-        type=int,
+        metavar="STR",
+        type=str,
         nargs="?",
         default=DEFAULT_CURRENCY_COLUMN,
-        help=f"index of the column containing the currency of the market value, default `{DEFAULT_CURRENCY_COLUMN}`.",
+        help=f"name of the column containing the currency of the market value, default `{DEFAULT_CURRENCY_COLUMN}`.",
     )
     csv_columns_group.add_argument(
         "--isin-column",
-        metavar="INT",
-        type=int,
+        metavar="STR",
+        type=str,
         nargs="?",
         default=DEFAULT_ISIN_COLUMN,
-        help=f"index of the column containing the isin of the component, default `{DEFAULT_ISIN_COLUMN}`.",
+        help=f"name of the column containing the isin of the component, default `{DEFAULT_ISIN_COLUMN}`.",
     )
+    # CSV parsing group
     csv_parsing_group = parser.add_argument_group("CSV parsing", "Details about CSV parsing process.")
-    csv_parsing_group.add_argument(
-        "--consider-first-line",
-        default=False,
-        action="store_true",
-        help="use this when the first line of the CSV contains data instead of an header.",
-    )
     csv_parsing_group.add_argument(
         "-d",
         "--delimiter",
@@ -198,66 +195,72 @@ def main():
     args = parser.parse_args()  # Parse command line arguments
     logging.basicConfig(level=args.loglevel[0].upper())
 
-    tickers: Dict[str, str] = {}  # Maps component ISIN to tickers
     # Load CSV file
-    with open(args.filename[0], mode="r", newline="", encoding="utf-8", errors="?") as csvfile:
-        if args.consider_first_line is False:
-            csvfile.readline()  # Skip the first CSV header row
-        with open(args.output, mode="w") as outfile:
-            reader = csv.reader(csvfile, delimiter=args.delimiter, quotechar=args.quotechar)
-            for row in reader:
-                # TODO: pass twice: save the isin the first time and the second time map non-existing
-                # end_nodes to the isin
-                start_node: str = row[args.etf_ticker_column]
-                end_node: str = row[args.component_ticker_column]
-                weight: str = row[args.market_value_column]
-                currency: str = row[args.currency_column]
-                isin: str = row[args.isin_column]
-                # Skip if None or empty
-                if not all([start_node, end_node, weight]):
-                    continue
-                # Discard indices
-                if start_node[0] == ".":
-                    continue
-                # Remove spaces from names
-                if " " in start_node:
-                    start_node = start_node.split(" ")[0]
-                if " " in end_node:
-                    end_node = end_node.split(" ")[0]
-                # Skip if value is invalid
-                if not all(re.match(TICKER_REGEX, value) for value in [start_node, end_node]) or not re.match(
-                    WEIGHT_REGEX, weight
-                ):
-                    if not isin or isin not in tickers:
-                        logger.debug(
-                            f"discarded {[start_node, end_node, weight, currency]}: TICKER_REGEX or \
-                            WEIGHT_REGEX did not match"
-                        )
-                        continue
-                    else:
-                        # Assing a ticker found in previous ETFs
-                        end_node = tickers[isin]
-                elif isin and re.match(ISIN_REGEX, isin):
-                    # Store the ticker for later use
-                    tickers[isin] = end_node
-                # Remove self-loop edges
-                if start_node == end_node:
-                    # NOTE: this means the node is an ETF
-                    continue
-                # Parse the weight to float
-                try:
-                    weight = float(weight)
-                except ValueError:
-                    logger.debug(f"discarded {[start_node, end_node, weight, currency]}: `{weight}` is not a float")
-                    continue
-                # Convert weight in non-EUR to EUR
-                if not args.no_currency_conversion:
-                    # Check currency
-                    if not re.match("([a-zA-Z]+){1,4}", currency):
-                        logger.debug(f"converted currency `{currency}` to {args.default_currency[0]}")
-                        currency = args.default_currency[0]
-                    weight = to_euros(weight, currency)
-                outfile.write(f"{start_node} {end_node} {weight}\n")
+    df = pd.read_csv(args.filename[0], sep=args.delimiter, quotechar=args.quotechar, encoding="utf-8", na_values=" ")
+    # Filter out the unwanted columns
+    WANTED_COLUMNS = [
+        args.etf_ticker_column,
+        args.component_ticker_column,
+        args.market_value_column,
+        args.currency_column,
+        args.isin_column,
+    ]
+    df = df[WANTED_COLUMNS]
+    print("After columns filter\n", df.describe())
+    # Fill missing currency values
+    if not args.no_currency_conversion:
+        df.fillna(value={args.currency_column: args.default_currency[0]}, inplace=True)
+    print("After currency fillNaN\n", df.describe())
+    # Fill missing ticker values
+    df.loc[df[args.etf_ticker_column].isnull(), args.etf_ticker_column] = df.loc[
+        df[args.etf_ticker_column].isnull(), args.isin_column
+    ].map(
+        df.loc[df[args.isin_column].notnull()]
+        .groupby(args.isin_column)
+        .aggregate({args.etf_ticker_column: "first"})[args.etf_ticker_column]
+    )
+    print("After ticker fill\n", df.describe())
+    # Fill the missing isin values
+    df.loc[df[args.isin_column].isnull(), args.isin_column] = df.loc[
+        df[args.isin_column].isnull(), args.etf_ticker_column
+    ].map(
+        df.loc[df[args.etf_ticker_column].notnull()]
+        .groupby(args.etf_ticker_column)
+        .aggregate({args.isin_column: "first"})[args.isin_column]
+    )
+    print("After isin fill\n", df.describe())
+    # Filter out the invalid tickers
+    df = df[
+        (df[args.etf_ticker_column].notnull() | df[args.etf_ticker_column].str.match(TICKER_REGEX))
+        & (df[args.component_ticker_column].notnull() | df[args.component_ticker_column].str.match(TICKER_REGEX))
+    ]
+    print("After invalid tickers removal\n", df.describe())
+    # Remove indices (whose ticker start with .)
+    df = df[df[args.etf_ticker_column].str.startswith(".") is False]
+    print("After indices removal\n", df.describe())
+    # Remove ticker location (like .JP .AU .HK )
+    df[args.etf_ticker_column] = df[args.etf_ticker_column].str.replace(r"\..*", "", regex=True)
+    df[args.component_ticker_column] = df[args.component_ticker_column].str.replace(r"\..*", "", regex=True)
+    print("After ticker location removal\n", df.describe())
+    # Rename market value column to weight
+    df.rename(columns={args.market_value_column: "weight"}, inplace=True)
+    # Create the networkx graph edges
+    G = nx.from_pandas_edgelist(
+        df,
+        source=args.etf_ticker_column,
+        target=args.component_ticker_column,
+        edge_attr="weight",
+        create_using=nx.DiGraph(),
+    )
+    # Add attributes to the nodes
+    # node_data = df[[args.component_ticker_column, args.isin_column]].drop_duplicates()
+    # print("Duplicated tickers\n", node_data[node_data.duplicated(
+    #    [args.component_ticker_column]) == True].sort_values(args.component_ticker_column).head(20))
+    # print("Node data\n", node_data.set_index(args.component_ticker_column).to_dict('index'))
+    #  isin attribute
+    # nx.set_node_attributes(G, node_data.set_index(args.component_ticker_column).to_dict('index'))
+    # Store the graph as GML file
+    nx.write_gml(G, f"{args.output[0]}.gml")
     print("Done!")
 
 
