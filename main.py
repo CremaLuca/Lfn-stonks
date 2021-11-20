1e7a379deb54853a94b8e414f56a648d72421a01
Riccardo Crociani <riccardo.crociani10@gmail.com>
2021-11-20 12:46:16 +0100

    feat: addded computation of clustering coefficient and closeness centrality

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
 import networkx as nx
+import timeit
+from networkx.algorithms import centrality
+from networkx.algorithms.shortest_paths.weighted import negative_edge_cycle
+from networkx.classes.function import subgraph
+from networkx.generators import directed
+import numpy as np
+import scipy
+import sys
 
-def load_graph(filename : str = "out_graph.gml"):
+def load_graph(filename : str):
     return nx.read_gml(filename)
 
 def capitalization_attribute(G):
@@ -32,22 +40,76 @@ def betweenness_centrality(G, percentage : float = 0.1, normalized:bool = True):
 def closeness_centrality(G):
     pass # Networkx does not have approximate algorithm
 
+def random_subgraph(G, n_nodes):
+    # Returns a random subgraph of G with n_nodes nodes and no negative cycles
+
+    sub_graph = subgraph(G, list(np.random.choice(list(G.nodes), n_nodes, replace=False)))
+    while negative_edge_cycle(nx.Graph(sub_graph)):
+        sub_graph = subgraph(G, list(np.random.choice(list(G.nodes), n_nodes, replace=False)))
+    return sub_graph
+
+
+def random_graph(n, p):
+    
+    #Returns a $G_{n,p}$ random graph, also known as an Erdős-Rényi graph or
+    #a binomial graph.
+    G = nx.fast_gnp_random_graph(n, p, directed=True)
+    return G
+
+#TODO: Associate the label of each node (instead of an int) with its cc
+def closeness_centrality_matrix(G):
+    A = nx.adjacency_matrix(G).tolil() #matrix converted into list of lists
+    D = scipy.sparse.csgraph.floyd_warshall(A, directed=False, unweighted=False)
+
+    n = D.shape[0]
+    centralities = {}
+    for r in range(0, n):
+    
+        cc = 0.0
+    
+        possible_paths = list(enumerate(D[r, :]))
+        shortest_paths = dict(filter( \
+            lambda x: not x[1] == np.inf, possible_paths))
+    
+        total = sum(shortest_paths.values())
+        n_shortest_paths = len(shortest_paths) - 1.0
+        if total > 0.0 and n > 1:
+            s = n_shortest_paths / (n - 1)
+            cc = (n_shortest_paths / total) * s
+        centralities[r] = cc
+    return centralities
+
 def clustering_coefficient(G):
     return nx.clustering(G, weight="weight")
 
-
-
 def main():
     print("Loading graph")
-    G = load_graph()
+    G = load_graph(sys.argv[1])
     capitalization_attribute(G)
     print("Graph loaded:\n", G)
     k = 20
+
+    print("-----------------------------------------------")
     print(f"Top {k} nodes with highest capitalization: {max_k_capitalization(G, k)}")
+    print("-----------------------------------------------")
+
     b_centralities = betweenness_centrality(G, percentage=0.02)
-    print(sorted(b_centralities.items(), key=lambda t: t[1], reverse=True)[:20])
-    #clustering_coeff = clustering_coefficient(G)
-    #print(sorted(clustering_coeff.items(), key=lambda t: t[1], reverse=True)[:20])
+    print(sorted(b_centralities.items(), key=lambda t: t[1], reverse=True)[:k])
+
+    print("-----------------------------------------------")
+    print("Clustering coefficient")
+    print("-----------------------------------------------")
+    clustering_coeff = clustering_coefficient(G)
+    print(sorted(clustering_coeff.items(), key=lambda t: t[1], reverse=True)[:k])
+    print("-----------------------------------------------")
+
+    
+    print("CLOSENESS MATRIX")
+    start = timeit.default_timer()
+    print(closeness_centrality_matrix(random_subgraph(G, int(0.1*G.number_of_nodes()))))
+    end = timeit.default_timer()
+    print(f"Time elapsed: {end - start}")
+    
 
 if __name__ == "__main__":
     main()
